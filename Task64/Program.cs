a314a8f06ff84cef8af62a5b49b41d36267deb32
EfimovE <efimov_e@mail.ru>
2022-09-08 19:43:44 +0500

    save01

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+void NatNumber(int num, int count)
+{
+  if (count > num) return;
+  NatNumber(num, count + 1);
+  Console.Write($"{count} ");
+}
+
+
+Console.WriteLine("Введите число N и нажмите Enter: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NatNumber(num, count);
+
